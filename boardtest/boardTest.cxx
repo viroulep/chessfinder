2de2dc7c8ecd02256ed1e981385e376bf93fa9d2
Philippe Virouleau <philippe.virouleau@imag.fr>
2014-02-01 15:15:34 +0100

    Added some board test.

--- boardtest/boardTest.cxx
+++ boardtest/boardTest.cxx
@@ -22,16 +22,148 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
-#include "Chessboard.h"
-#include "Hashing.h"
-#include "Utils.h"
-#include "Options.h"
+#include "SimpleChessboard.h"
+#include "Movegen.h"
+/*#include "Hashing.h"*/
+/*#include "Utils.h"*/
+/*#include "Options.h"*/
 
 using namespace std;
 using namespace Board;
 
+
+#define DISPLAY_SET(setName) \
+cout << "Display :";\
+for (Square s : setName) {\
+    cout << " " << square_to_string(s);\
+}\
+cout << "\n";
+
+#define TEST_ATTACK(target, color, setName) \
+    setName = Board::gen_attacker(color, target, pos);\
+cout << "Attacking " << square_to_string(target) << " as "\
+<< color_to_string(color) << "\n";\
+DISPLAY_SET(setName);
+
+#define TEST_MOVE(rank, file, piece, setName) \
+    pos.board_[make_square(rank, file)] = piece;\
+switch (kind_of(piece)) {\
+    case KING:\
+        setName = Board::gen_reachable<KING>(make_square(rank, file), pos);\
+    break;\
+    case QUEEN:\
+        setName = Board::gen_reachable<QUEEN>(make_square(rank, file), pos);\
+    break;\
+    case BISHOP:\
+        setName = Board::gen_reachable<BISHOP>(make_square(rank, file), pos);\
+    break;\
+    case KNIGHT:\
+        setName = Board::gen_reachable<KNIGHT>(make_square(rank, file), pos);\
+    break;\
+    case ROOK:\
+        setName = Board::gen_reachable<ROOK>(make_square(rank, file), pos);\
+    break;\
+    case PAWN:\
+        setName = Board::gen_reachable<PAWN>(make_square(rank, file), pos);\
+    break;\
+    default:\
+            break;\
+}\
+cout << "Reachable :";\
+for (Square s : setName) {\
+    cout << " " << square_to_string(s);\
+    pos.board_[s] = piece;\
+}\
+cout << "\n";\
+cout << pos.pretty() << endl;\
+pos.set(startFen);
+
+
 int main()
 {
+    cout << "Sizeof struct : " << sizeof(Square) << "\n";
+    cout << "b2 ? :" << to_string(SQ_B4 & 7) << ":" << to_string(SQ_B4 >> 3) << "\n";
+    string startFen = "8/8/7k/1Q2P3/7K/q7/8/1R4b1 w - f6 0 1";
+    Position pos;
+    pos.set(startFen);
+    cout << pos.pretty() << endl;
+
+    set<Square> gen;
+
+    cout << "Knight :\n";
+    TEST_MOVE(RANK_6, FILE_B, W_KNIGHT, gen);
+
+    cout << "Bishop :\n";
+    TEST_MOVE(RANK_1, FILE_A, W_BISHOP, gen);
+    TEST_MOVE(RANK_2, FILE_B, W_BISHOP, gen);
+    TEST_MOVE(RANK_5, FILE_C, W_BISHOP, gen);
+
+    cout << "Rook :\n";
+    TEST_MOVE(RANK_1, FILE_A, W_ROOK, gen);
+    TEST_MOVE(RANK_2, FILE_B, W_ROOK, gen);
+    TEST_MOVE(RANK_5, FILE_C, W_ROOK, gen);
+    TEST_MOVE(RANK_3, FILE_D, B_ROOK, gen);
+
+    cout << "Queen :\n";
+    TEST_MOVE(RANK_1, FILE_A, W_QUEEN, gen);
+    TEST_MOVE(RANK_2, FILE_B, W_QUEEN, gen);
+    TEST_MOVE(RANK_5, FILE_C, W_QUEEN, gen);
+    TEST_MOVE(RANK_3, FILE_A, B_QUEEN, gen);
+    TEST_MOVE(RANK_3, FILE_D, B_QUEEN, gen);
+
+    cout << "King :\n";
+    TEST_MOVE(RANK_1, FILE_A, W_KING, gen);
+    TEST_MOVE(RANK_2, FILE_B, W_KING, gen);
+    TEST_MOVE(RANK_5, FILE_C, W_KING, gen);
+
+    cout << "Pawn :\n";
+    TEST_MOVE(RANK_7, FILE_F, W_PAWN, gen);
+    TEST_MOVE(RANK_5, FILE_H, W_PAWN, gen);
+    TEST_MOVE(RANK_2, FILE_G, W_PAWN, gen);
+    TEST_MOVE(RANK_2, FILE_H, W_PAWN, gen);
+    TEST_MOVE(RANK_2, FILE_B, W_PAWN, gen);
+    TEST_MOVE(RANK_6, FILE_C, B_PAWN, gen);
+
+    cout << pos.pretty();
+    cout << "Test attack :\n";
+    TEST_ATTACK(make_square(RANK_1, FILE_G), WHITE, gen);
+    TEST_ATTACK(make_square(RANK_1, FILE_F), WHITE, gen);
+    TEST_ATTACK(make_square(RANK_4, FILE_D), WHITE, gen);
+    TEST_ATTACK(make_square(RANK_4, FILE_D), BLACK, gen);
+    TEST_ATTACK(make_square(RANK_5, FILE_C), WHITE, gen);
+    TEST_ATTACK(make_square(RANK_5, FILE_C), BLACK, gen);
+
+
+#if 0
+    string startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    pos.set(startFen);
+    cout << pos.pretty() << endl;
+    cout << "import : " << startFen << "\n";
+    cout << "export : " << pos.fen() << "\n";
+
+    string startFen2 = "rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2";
+    pos.set(startFen2);
+    cout << pos.pretty();
+    cout << "import : " << startFen2 << "\n";
+    cout << "export : " << pos.fen() << "\n";
+
+    string fen = "2r5/3P4/8/8/5K2/8/3k4/8 w - - 0 38";
+    pos.set(fen);
+    cout << pos.pretty();
+    cout << "import : " << fen << "\n";
+    cout << "export : " << pos.fen() << "\n";
+    set<Square> gen = Board::gen_reachable<KNIGHT>(make_square(RANK_6, FILE_B), pos);
+    cout << "Reachable :";
+    for (Square s : gen)
+        cout << " " << square_to_string(s);
+    cout << "\n";
+    gen = Board::gen_reachable<KNIGHT>(make_square(RANK_4, FILE_E), pos);
+    cout << "Reachable :";
+    for (Square s : gen)
+        cout << " " << square_to_string(s);
+    cout << "\n";
+
+
     Options::setVerboseLevel(2);
     //string fen = "rnbqkbnr/pp2pppp/8/2ppP3/8/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 3";
     /*string startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";*/
@@ -239,4 +371,5 @@ int main()
     //Utils::output(test->to_string() + "\n");
     delete test;
     return 0;
+#endif
 }
